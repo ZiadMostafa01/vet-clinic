46381f7af4ca86c5cb8c6aa132d507cd862b00f9
ZiadMostafa01 <ziadmostafa9866@gmail.com>
2026-02-19 14:32:21 +0200

    Update

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter basename="/pet-clinic/">
+    <BrowserRouter basename="/vet-clinic/">
       <App />
     </BrowserRouter>
   </React.StrictMode>,
